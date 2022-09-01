1af85347c32c2a2f12b6cff427dd9e3ced32f010
hritik <hritikc3961@gmail.com>
2022-09-01 09:46:04 -0700

    final

--- android/app/src/main/kotlin/com/example/hvac_dashboard/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.hvac_dashboard
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/com/example/untitled/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_hvac
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
